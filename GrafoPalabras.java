a514f8024a2dbee555679c74566060bc57a1c28c
hupog <110995992+hupog@users.noreply.github.com>
2022-11-13 02:34:43 +0100

    Update GrafoPalabras.java

--- GrafoPalabras.java
+++ GrafoPalabras.java
@@ -3,32 +3,58 @@ package p6;
 import java.util.*;
 import grafo.*;
 
-public class GrafoPalabras {
-    Map<String, List<String>> m;
-    ListaAdyacencia<String, Integer> grafoPalabras; 
-  
-    public void insertarPalabra(String p) {
-        List<String> aux = new ArrayList();
-        for (int i = 0; i < p.length(); i++) {
-            String clave = reemplazaCaracter(p, i, '_');
-            if (!m.containsKey(clave)) {
-                aux.add(p);
-                m.put(clave, aux);
-                aux.clear();
-            } else {
-                aux = m.get(clave);
-                aux.add(",");
-                aux.add(p);
-                m.put(clave, aux);
-                aux.clear();
+public final class GrafoPalabras {
+
+    Map<String, List<String>> palabras;
+    ListaAdyacencia<String, Integer> grafoPalabras;
+    
+    public GrafoPalabras(List<String> palabras){
+        insertarPalabras(palabras);
+        insertarVertices(palabras);
+        insertarArcos();
+    }
+    
+    public void insertarArcos() {
+        for (String clave : palabras.keySet()) {
+            Iterator<Vertice<String>> it = grafoPalabras.vertices();
+        }
+    }
+    
+    public void insertarVertices(List<String> p){
+        for(String palabra : p){
+            Vertice<String> aux;
+            aux.setEtiqueta(palabra);
+            if(!grafoPalabras.estaVertice(aux))
+                grafoPalabras.insertarVertice(aux);
+        }
+    }
+
+    //Suponemos que las palabras estan separadas por una coma ','
+    public void insertarPalabras(List<String> p) throws IllegalArgumentException {
+        if (p.isEmpty()) {
+            throw new IllegalArgumentException("Lista vacía");
+        } else {
+            for (String palabra : p) {
+                List<String> aux = new ArrayList();
+                for (int i = 0; i < palabra.length(); i++) {
+                    String clave = reemplazaCaracter(palabra, i, '_');
+                    if (!palabras.containsKey(clave)) {
+                        aux.add(palabra);
+                        palabras.put(clave, aux);
+                        aux.clear();
+                    } else {
+                        aux = palabras.get(clave);
+                        aux.add(",");
+                        aux.add(palabra);
+                        palabras.put(clave, aux);
+                        aux.clear();
+                    }
+                }
             }
         }
     }
 
-    public void rellenarGrafo(){
-        
-    }
-    //Reemplaza en la posicion que le pases un caracter de la palabra, ejemplo: reemplazarCaracter(Hola, 1, '_') = H_la
+    //Reemplaza el caracter de la posicion indicada por otro caracter que quieras, en este caso lo uso para sustituir por '_' para la clave
     public static String reemplazaCaracter(String str, int pos, char letra) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < str.length(); i++) {
@@ -41,3 +67,5 @@ public class GrafoPalabras {
         return sb.toString();
     }
 }
+    
+
